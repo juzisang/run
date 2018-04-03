73b3011aabe3acc0c30b998557e82e496b620265
luofeng <1915327117@qq.com>
2018-04-03 18:25:34 +0800

    增加vue模版

---
+++ src/modules/vue/init.js
@@ -0,0 +1,43 @@
+const inquirer = require('inquirer')
+const ModernBase = require('../../core/base/ModernBase')
+
+class VueInit extends ModernBase {
+  onStart (config) {
+
+    let questions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Project name：',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Project description',
+        default: 'A Vue.js project'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author'
+      },
+      {
+        type: 'confirm',
+        name: 'installVueRouter',
+        message: 'Install vue-router'
+      },
+      {
+        type: 'confirm',
+        name: 'eslint',
+        message: 'Use ESLint to lint your code?'
+      }
+    ]
+    inquirer.prompt(questions)
+  }
+
+  onRun (config) {
+  }
+}
+
+module.exports = VueInit
